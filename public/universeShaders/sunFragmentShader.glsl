0c27a31c0b6fda503e699c6f20061ba418e4f496
Martin Mrazek <mrazekmart@gmail.com>
2023-06-22 20:46:38 +0200

    -huge universe buff

--- public/universeShaders/sunFragmentShader.glsl
+++
@@ -1,8 +0,0 @@
-uniform float time;
-varying vec2 vUv;
-varying vec3 vPosition;
-uniform samplerCube uPerlin;
-
-void main() {
-    gl_FragColor = textureCube(uPerlin, vPosition);
-}
--- src/components/background/MMUniverse.tsx
+++ src/components/background/MMUniverse.tsx
@@ -12,6 +12,7 @@ interface Planet {
     size: number;
     color: number[];
     position: number[];
+    distanceFromSun: number;
     moons: Moon[];
     hasRings: boolean;
 }
@@ -33,29 +34,48 @@ interface Moon {
 const MMUniverse = () => {
     const canvasRef: React.RefObject<HTMLCanvasElement> = useRef(null);
 
-    const [zoom, setZoom] = useState<number>(1);
     const [solarSystem, setSolarSystem] = useState<SolarSystem | undefined>();
     const [maxWidth] = useState<number>(5000);
     const [maxHeight] = useState<number>(5000);
 
-    const [sunVShader, setSunVShader] = useState("");
-    const [sunFShader, setSunFShader] = useState("");
+    const [planetVShader, setPlanetVShader] = useState("");
+    const [planetFShader, setPlanetFShader] = useState("");
+    const [cloudVShader, setCloudVShader] = useState("");
+    const [cloudFShader, setCloudFShader] = useState("");
     const [cubeVShader, setCubeVShader] = useState("");
     const [cubeFShader, setCubeFShader] = useState("");
+    const [moonVShader, setMoonVShader] = useState("");
+    const [moonFShader, setMoonFShader] = useState("");
     const [loading, setLoading] = useState(true);
+
+    const speed = useRef(0);
+    const direction = useRef(new THREE.Vector3());
+
+    const isDragging = useRef(false);
+    const startPosition = useRef({x: 0, y: 0});
+    const currentPosition = useRef({x: 0, y: 0});
+
 
     useEffect(() => {
         Promise.all([
-            fetch('/universeShaders/sunVertexShader.glsl').then(response => response.text()),
-            fetch('/universeShaders/sunFragmentShader.glsl').then(response => response.text()),
+            fetch('/universeShaders/planetVShader.glsl').then(response => response.text()),
+            fetch('/universeShaders/planetFShader.glsl').then(response => response.text()),
             fetch('/universeShaders/cubeVertexShader.glsl').then(response => response.text()),
-            fetch('/universeShaders/cubeFragmentShader.glsl').then(response => response.text())
+            fetch('/universeShaders/cubeFragmentShader.glsl').then(response => response.text()),
+            fetch('/universeShaders/cloudVShader.glsl').then(response => response.text()),
+            fetch('/universeShaders/cloudFShader.glsl').then(response => response.text()),
+            fetch('/universeShaders/moonVShader.glsl').then(response => response.text()),
+            fetch('/universeShaders/moonFShader.glsl').then(response => response.text())
         ])
-            .then(([sunVShaderData, sunFShaderData, cubeVShaderData, cubeFShaderData]) => {
-                setSunVShader(sunVShaderData);
-                setSunFShader(sunFShaderData);
+            .then(([sunVShaderData, sunFShaderData, cubeVShaderData, cubeFShaderData, cloudVShaderData, cloudFShaderData, moonVShaderData, moonFShaderData]) => {
+                setPlanetVShader(sunVShaderData);
+                setPlanetFShader(sunFShaderData);
                 setCubeVShader(cubeVShaderData);
                 setCubeFShader(cubeFShaderData);
+                setCloudVShader(cloudVShaderData);
+                setCloudFShader(cloudFShaderData);
+                setMoonVShader(moonVShaderData);
+                setMoonFShader(moonFShaderData);
                 setLoading(false);
             })
             .catch((error) => {
@@ -74,6 +94,9 @@ const MMUniverse = () => {
 
         if (!canvas) return;
 
+        canvas.setAttribute('tabindex', '0');
+        canvas.focus();
+
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
 
@@ -81,13 +104,13 @@ const MMUniverse = () => {
 
         if (loading) return;
 
-        renderSolarSystem(solarSystem, canvas, zoom);
+        renderSolarSystem(solarSystem, canvas);
 
 
-    }, [solarSystem, zoom, sunVShader]);
+    }, [solarSystem, planetVShader]);
 
     function generateSolarSystem() {
-        let sunSize = 100;
+        let sunSize = 200;
 
         const solarSystem = {
             mainStar: {
@@ -99,7 +122,7 @@ const MMUniverse = () => {
             stars: [] as Star[],
         };
 
-        const numPlanets = Math.floor(Math.random() * 6) + 4;
+        const numPlanets = Math.floor(Math.random() * 3) + 8;
 
         for (let i = 0; i < numPlanets; i++) {
             let planetSize = getRandomSize(15, 65);
@@ -111,6 +134,7 @@ const MMUniverse = () => {
                 size: planetSize,
                 color: getRandomColor(),
                 position: planetPosition,
+                distanceFromSun: distance,
                 moons: [] as Moon[],
                 hasRings: Math.random() < 0.5,
             };
@@ -167,49 +191,28 @@ const MMUniverse = () => {
         return solarSystem;
     }
 
-    const generateNoiseTexture = () => {
-        const size = 512;
-        const data = new Uint8Array(size * size * 3);
-
-        for (let i = 0; i < size * size; i++) {
-            const stride = i * 3;
-            const value = Math.random() * 255;
-
-            data[stride] = value;
-            data[stride + 1] = value;
-            data[stride + 2] = value;
-        }
-
-        const texture = new THREE.DataTexture(data, size, size, THREE.RGBAFormat);
-        texture.wrapS = THREE.RepeatWrapping;
-        texture.wrapT = THREE.RepeatWrapping;
-        texture.needsUpdate = true;
-
-        return texture;
-    };
-    const noiseTexture = generateNoiseTexture();
-
     function renderSolarSystem(
         solarSystem: { mainStar: Star; planets: Planet[]; stars: Star[] },
-        canvas: HTMLCanvasElement,
-        zoomLevel: number
+        canvas: HTMLCanvasElement
     ) {
+        const clock = new THREE.Clock();
 
         const renderer = new THREE.WebGLRenderer({
             canvas,
-            context: canvas.getContext("webgl2") as WebGLRenderingContext
+            context: canvas.getContext("webgl2") as WebGLRenderingContext,
+            antialias: true
         });
         renderer.setSize(canvas.width, canvas.height);
         renderer.setClearColor(0x000000);
         renderer.clear();
         const scene = new THREE.Scene();
         const camera = new THREE.PerspectiveCamera(
-            200,
+            75,
             canvas.width / canvas.height,
             0.1,
-            1000
+            3000
         );
-        camera.position.z = 300 * zoomLevel;
+        camera.position.setZ(2000);
 
         for (let i = 0; i < solarSystem.stars.length; i++) {
             const currStar = solarSystem.stars[i];
@@ -222,16 +225,11 @@ const MMUniverse = () => {
         }
 
 
-
-        let cubeRenderTarget = new THREE.WebGLCubeRenderTarget( 256 );
+        let cubeRenderTarget = new THREE.WebGLCubeRenderTarget(256);
         cubeRenderTarget.texture.type = THREE.HalfFloatType;
 
-        let cubeCamera = new THREE.CubeCamera( 1, 1000, cubeRenderTarget );
 
-
-
-        //const sunMaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
-        const materialPerlin = new THREE.ShaderMaterial({
+        const sunMaterial = new THREE.ShaderMaterial({
             uniforms: {
                 time: {value: 0.0}
             },
@@ -240,88 +238,167 @@ const MMUniverse = () => {
         });
 
         const geometry = new THREE.SphereGeometry(200, 30, 30);
-        const perlin = new THREE.Mesh(geometry, materialPerlin);
-        perlin.position.set(0, 0, 10);
-        scene.add(perlin);
-
-
-        function animate() {
-            requestAnimationFrame(animate);
-            materialPerlin.uniforms.time.value += 0.0008;
-            renderer.render(scene, camera);
-            cubeCamera.update(renderer, scene);
-        }
-
+        const sun = new THREE.Mesh(geometry, sunMaterial);
+        scene.add(sun);
 
         const numPlanets = solarSystem.planets.length;
+
+        const cloudMaterials: any = [];
 
         for (let i = 0; i < numPlanets; i++) {
             const planet = solarSystem.planets[i];
 
-            const planetColor = new THREE.Color(planet.color[0], planet.color[1], planet.color[2]);
-            //const planetColor = new THREE.Color(1, 1, 1);
-            const planetGeometry = new THREE.CircleGeometry(planet.size, 32);
+            const planetGeometry = new THREE.SphereGeometry(planet.size, 32);
             const planetMaterial = new THREE.ShaderMaterial({
-                vertexShader: `
-                    void main() {
-                      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-                    }
-                  `,
-                fragmentShader: `
-                    uniform vec3 sunColor;
-                    uniform float sunIntensity;
-                    uniform vec2 sunPosition;
-            
-                    void main() {
-                      float distance = length(gl_FragCoord.xy - sunPosition);
-                      float brightness = 1.0;
-                      vec3 finalColor = sunColor * sunIntensity * brightness;
-                      gl_FragColor = vec4(finalColor, 1.0);
-                    }
-                  `,
                 uniforms: {
-                    sunColor: {value: planetColor},
-                    sunIntensity: {value: 1.0},
-                    sunPosition: {value: new THREE.Vector2(0, 0)},
+                    pOffset: {value: getRandomFloat(0, 100000)}
                 },
+                vertexShader: planetVShader,
+                fragmentShader: planetFShader
             });
+
+            if(planet.distanceFromSun < 600){
+                planetMaterial.fragmentShader = moonFShader;
+            }
+
             const planetMesh = new THREE.Mesh(planetGeometry, planetMaterial);
             planetMesh.position.set(planet.position[0], planet.position[1], 0);
             scene.add(planetMesh);
 
+            const cloudGeometry = new THREE.SphereGeometry(planet.size + 1, 32);
+            const cloudMaterial = new THREE.ShaderMaterial({
+                uniforms: {
+                    pOffset: {value: getRandomFloat(0, 100000)},
+                    time: {value: 0.0}
+                },
+                vertexShader: cloudVShader,
+                fragmentShader: cloudFShader,
+                transparent: true,
+            });
+            cloudMaterials.push(cloudMaterial);
+
+            const clouds = new THREE.Mesh(cloudGeometry, cloudMaterial);
+            clouds.position.set(planet.position[0], planet.position[1], 0);
+            scene.add(clouds);
 
             const numMoons = planet.moons.length;
 
             for (let j = 0; j < numMoons; j++) {
                 const moon = planet.moons[j];
-                const moonGeometry = new THREE.CircleGeometry(moon.size, 32);
+                const moonGeometry = new THREE.SphereGeometry(moon.size, 32);
                 const moonColor = new THREE.Color(moon.color[0], moon.color[1], moon.color[2]);
-                const moonMaterial = new THREE.MeshBasicMaterial({color: moonColor});
+                const moonMaterial = new THREE.ShaderMaterial({
+                    uniforms: {
+                        pOffset: {value: getRandomFloat(0, 100000)},
+                        color: {value: moonColor}
+                    },
+                    vertexShader: moonVShader,
+                    fragmentShader: moonFShader
+                });
                 const moonMesh = new THREE.Mesh(moonGeometry, moonMaterial);
                 moonMesh.position.set(moon.position[0], moon.position[1], 0);
                 scene.add(moonMesh);
             }
         }
+
+        let timer: number = 0;
+
+        function animate() {
+
+            requestAnimationFrame(animate);
+            const delta: number = clock.getDelta();
+            timer += delta;
+            if (timer < 0.02) {
+                return;
+            }
+            timer = 0;
+
+            camera.getWorldDirection(direction.current);
+
+            camera.position.addScaledVector(direction.current, speed.current);
+
+
+            if (isDragging.current) {
+                const dx = (currentPosition.current.x - startPosition.current.x) * 0.001;
+                const dy = (currentPosition.current.y - startPosition.current.y) * 0.001;
+
+                camera.rotation.y += dx;
+                camera.rotation.x += dy;
+
+                startPosition.current = currentPosition.current;
+            }
+
+
+            sunMaterial.uniforms.time.value += 0.004;
+            if (cloudMaterials.length > 0) {
+                cloudMaterials.forEach((item: any, index: number) => {
+                    item.uniforms.time.value += 0.004;
+                });
+            }
+            renderer.render(scene, camera);
+            //cubeCamera.update(renderer, scene);
+        }
+
+        function getRandomFloat(min: number, max: number) {
+            return Math.random() * (max - min) + min;
+        }
+
         animate();
+
+        const handleKeyDown = (event: KeyboardEvent) => {
+            if (event.code === "KeyW") {
+                speed.current = 2;
+            } else if (event.code === "KeyS") {
+                speed.current = -2;
+            } else if (event.code === "KeyA") {
+                camera.position.x += 2;
+            } else if (event.code === "KeyD") {
+                camera.position.x -= 2;
+            }
+        };
+
+        const handleKeyUp = (event: KeyboardEvent) => {
+            if (event.code === "KeyW") {
+                speed.current = 0;
+            } else if (event.code === "KeyS") {
+                speed.current = 0;
+            }
+        };
+
+        const handleMouseDown = (event: MouseEvent) => {
+            isDragging.current = true;
+            startPosition.current = {x: event.clientX, y: event.clientY};
+        };
+
+        const handleMouseUp = (event: MouseEvent) => {
+            isDragging.current = false;
+        };
+
+        const handleMouseMove = (event: MouseEvent) => {
+            if (isDragging.current) {
+                currentPosition.current = {x: event.clientX, y: event.clientY};
+            }
+        };
+
+        canvas.addEventListener('keydown', handleKeyDown);
+        canvas.addEventListener('keyup', handleKeyUp);
+
+        canvas.addEventListener('mousedown', handleMouseDown, false);
+        canvas.addEventListener('mouseup', handleMouseUp, false);
+        canvas.addEventListener('mousemove', handleMouseMove, false);
+        return () => {
+            canvas.removeEventListener('keydown', handleKeyDown);
+            canvas.removeEventListener('keyup', handleKeyUp);
+            canvas.removeEventListener('mousedown', handleMouseDown, false);
+            canvas.removeEventListener('mouseup', handleMouseUp, false);
+            canvas.removeEventListener('mousemove', handleMouseMove, false);
+        };
     }
 
-    const handleZoom: React.WheelEventHandler<HTMLDivElement> = (event => {
-        const delta = event.deltaY;
-        const zoomFactor = 0.003;
-
-        setZoom((prevZoom) => {
-            const newZoom = prevZoom + delta * zoomFactor;
-            return Math.max(0.1, newZoom);
-        });
-    });
-
     return (
-        <div style={{position: 'relative'}}
-             onWheel={handleZoom}
-        >
+        <div style={{position: 'relative'}}>
             <canvas ref={canvasRef} style={{position: 'fixed', top: 0, left: 0}}/>
         </div>
     );
 }
-
 export default MMUniverse;
